709cfdbc387f3b2142cbe0777413abeeb2dcce4e
dipito <77622897+Gi-Woong@users.noreply.github.com>
2022-08-16 03:11:27 +0900

    feature: 컨트렉트 추가 및 상속 추가

--- contracts/SettlementContract.sol
+++ contracts/SettlementContract.sol
@@ -16,32 +16,82 @@ contract Buyer {
     }
 }
 
-contract Uploader {
+contract CopyrightHolders {
     address public uploaderAddress;
-    uint public uploadSong;
+    address[] public sellerAddresses;
+    uint[] public proportion; 
+    uint public uploadedSong;
+    uint public sellerContractDate;
     uint public songPrice;
-    uint public uploaderContractDate;
 
-    event log(address _address);
+    function getsellerAddresses() public view returns(address[] memory) {
+        return sellerAddresses;
+    }
+    function getProportion() public view returns(uint[] memory) {
+        return proportion;
+    }
+}
 
-    constructor(address _address, uint toUpload, uint price) {
-        uploaderAddress = _address;
-        uploadSong = toUpload;
+contract Seller is CopyrightHolders {
+
+
+    event log(address[] _address);
+    constructor(address _uploaderAddress, address[] memory _addresses, uint[] memory _proportions, uint toUpload, uint price) {
+        uploaderAddress = _uploaderAddress;
+        sellerAddresses = _addresses;
+        proportion = _proportions;
+        uploadedSong = toUpload;
         songPrice = price;
-        uploaderContractDate = block.timestamp;
-        emit log(uploaderAddress);
+        sellerContractDate = block.timestamp;
+        emit log(sellerAddresses);
     }
 }
 
 contract Contract {
     Buyer buyer;
-    Uploader uploader;
+    Seller seller;
 
-    bool alreadySetBuyer = false;
-    bool alreadySetUploader = false;
+    address owner;
+    uint ContractDate;
+
+    bool private alreadySetBuyer = false;
+    bool private alreadySetSeller = false;
+    bool contractEnd = false;
+    uint private index = 0;    
+
+    modifier mustCheck() {
+        require(buyer.wantToBuy() == seller.uploadedSong(), "buyer.wantToBuy() != seller.uploadedSong()");
+        if (contractEnd)
+            alreadySetBuyer = false;
+        _;
+    }
+
+    modifier onlyOwner() {
+        _;
+    }
+
+    function setSeller(address[] memory addresses, uint[] memory proportions, uint songId, uint songPrice) public {
+        require(!alreadySetSeller, "alreadySetSeller");
+        seller = new Seller({
+            _uploaderAddress: msg.sender,
+            _addresses: addresses,
+            _proportions: proportions,
+            toUpload: songId,
+            price: songPrice
+        });
+
+        // for(uint i=0; i<addresses.length; i++) {
+        //     isWithdrew[]
+        // }
+        // 백엔드에서 업로드시 check해줄거기 때문에 
+        // proportion의 합이 10000인지 체크하지 않아도 됨.
+        alreadySetSeller = true;
+    }
 
     function setBuyer(uint songId) public {
         require(!alreadySetBuyer, "alreadySetBuyer");
+        contractEnd = false;
+
         buyer = new Buyer({
             _address: msg.sender,
             toBuy: songId
@@ -49,39 +99,48 @@ contract Contract {
         alreadySetBuyer = true;
     }
 
-    function setUploader(uint songId, uint songPrice) public {
-        require(!alreadySetUploader, "!alreadySetUploader");
-        uploader = new Uploader({
-            _address: msg.sender,
-            toUpload: songId,
-            price: songPrice
-        });
-        alreadySetUploader = true;
-    }
-
-    modifier mustCheck() {
-        require(buyer.wantToBuy() == uploader.uploadSong(), "buyer.wantToBuy() != uploader.uploadSong()");
-        _;
-    }
-
     function buyerSendMoney(uint amount) public payable mustCheck() {
+        require(amount%10000 == 0, "amount%10000 != 0");
         require(msg.sender == buyer.buyerAddress(), "msg.sender != buyer.buyerAddress()");
         require(msg.sender.balance > amount, "msg.sender.balance <= amount");
-        require(amount == uploader.songPrice(), "amount != uploader.songPrice()");
+        require(amount == seller.songPrice(), "amount != seller.songPrice()");
         require(amount == msg.value, "amount != msg.value");
-        require(amount == address(this).balance, "amount != address(this).balance");
+        // require(amount == address(this).balance, "amount != address(this).balance");
+    }
+    event log(address me);
+
+    function withdrawToSeller() public payable mustCheck() {
+        require(!contractEnd, "contractEnd.");
+        //require(msg.sender == seller.uploaderAddress(), "msg.sender != seller.sellerAddresses()");
+        // require(address(this).balance > 0, "address(this).balance <= 0" ); //0원일 때 호출 방지
+        address reciever = seller.sellerAddresses(index);
+        payable(reciever).transfer(address(this).balance);
+        index++;
+        if (index >= seller.getsellerAddresses().length) {
+            index = 0;
+            if(seller.songPrice() > 0)
+                require(address(this).balance % seller.songPrice() == 0, "something Wrong.");
+            contractEnd = true;
+        }
     }
 
-    function withdrawToUploader(uint amount) public payable mustCheck() {
-        require(msg.sender == uploader.uploaderAddress(), "msg.sender != uploader.uploaderAddress()");
-        require(amount == uploader.songPrice(), "amount != uploader.songPrice()");
-        require(address(this).balance > 0, "address(this).balance <= 0" ); //0원일 때 호출 방지
-        payable(msg.sender).transfer(address(this).balance);
-    }
+    // function withdrawByAdmin() public payable mustCheck() {
+    //     require(msg.sender == admin, "Only admin can call this function.");
+    //     payable(msg.sender).transfer(address(this).balance);
+    // }
 
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 
+    function destroy() public {
+        require(msg.sender == owner, "Only Owner can call this function.");
+        selfdestruct(payable(owner));
+    }
+
+    constructor() { 
+        owner = msg.sender; 
+        ContractDate = block.timestamp;
+    }
 }
 
