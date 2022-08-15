9860c05a4f9ad1de8e891a1b38cf9396150f7f27
dipito <77622897+Gi-Woong@users.noreply.github.com>
2022-08-16 03:57:12 +0900

    refactor: BuyerContract를 SettlementContract에 통합 등

--- contracts/SettlementContract.sol
+++ contracts/SettlementContract.sol
@@ -2,25 +2,25 @@
 pragma solidity ^0.8.0.0;
 
 import "./SellerContract.sol";
-import "./BuyerContract.sol";
 
 contract SettlementContract {
-    BuyerContract public buyer;
     SellerContract public seller;
 
     address private owner; //정상적이라면 seller가 됨.
-    uint public contractDate;
-
     bool private alreadySetBuyer = false; //거래가 끝날 때까지 true
     bool private alreadySetSeller = false; //한번 등록하면 영원히 true
     bool private contractEnd = false; //거래 종료 여부
-
     address[] private sellerAddresses; // 판매자(정산자) 지갑 주소
     uint[] private proportion; // 곡 정산 비율
+    // uint public contractDate;
+
+    // 구매자
+    address public buyerAddress;
+    uint public buyerMusicId; //음원 id
 
     modifier mustCheck() {
         require(!contractEnd, "contractEnd.");
-        require(buyer.musicId() == seller.uploadedSong(), "buyer.wantToBuy() != seller.uploadedSong()");
+        require(buyerMusicId == seller.uploadedSong(), "buyer.wantToBuy() != seller.uploadedSong()");
         _;
     }
 
@@ -38,7 +38,7 @@ contract SettlementContract {
 
     modifier modBuyerSendMoney(uint amount) {
         require(amount%10000 == 0, "amount%10000 != 0");
-        require(msg.sender == buyer.buyerAddress(), "msg.sender != buyer.buyerAddress()");
+        require(msg.sender == buyerAddress, "msg.sender != buyer.buyerAddress()");
         require(msg.sender.balance > amount, "msg.sender.balance <= amount");
         require(amount == seller.songPrice(), "amount != seller.songPrice()");
         require(amount == msg.value, "amount != msg.value");
@@ -48,17 +48,15 @@ contract SettlementContract {
     }
 
     // 주소 접근 방지
-    modifier modAfterContstruct(address msgsender) {
+    modi    fier modAfterContstruct(address msgsender) {
         require(msgsender == address(seller), "msgsender != address(seller)");
         _;
     }
 
     function setBuyer(uint songId) public modSetBuyer() {
         require(songId == seller.uploadedSong());
-        buyer = new BuyerContract({
-            _address: msg.sender,
-            _musicId: songId
-        });
+        buyerMusicId = songId;
+        buyerAddress = msg.sender;
     }
 
     function buyerSendMoney(uint amount) public payable mustCheck() modBuyerSendMoney(amount){
@@ -93,7 +91,7 @@ contract SettlementContract {
 
     constructor() { 
         owner = msg.sender; 
-        contractDate = block.timestamp;
+        // contractDate = block.timestamp;
         seller = SellerContract(msg.sender); //호출자의 주소를 Seller contract로 casting
     }
 }
