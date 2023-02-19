c5a924ac39acd8b562aeea8bce48f7fe980e737f
dipito <77622897+Gi-Woong@users.noreply.github.com>
2023-02-19 18:14:57 +0900

    test: 테스트코드 수정 (#10)
    * perf: web3.js 버젼 up
    
    * chore: gitignore .vscode 추가
    
    * test: 테스트 코드 수정

--- html test/js/service.js
+++ html test/js/service.js
@@ -1,58 +1,61 @@
 import * as newca from "./contracts_copyright.js";
- newca.init().then(() => {
-  document
-    .querySelector(".deployContracts")
-    .addEventListener("click", async () => {
-      await newca.init();
-      console.log("deployContracts button clicked");
-      // deploy SettlementContract              
-      const addresses = [
-        // "0x1Fb5Fd68e9b34F8aF64d3B5e2D80ad9Df96F703B",
-        // "0x9ba5f78235AB268e92615a2370a81aBB2E79C9eb",
-        "0x2D96D8CC704F0f19367725c65A05Fa5Fe373fEC1",
-        "0x2D96D8CC704F0f19367725c65A05Fa5Fe373fEC1"
-      ];
-      const proportions = [5000, 5000]; 
-      const SongCid = "QmWZipPtKnuVkn1mwNMqEw8apx513me2B3aCByCchsCmHk";
-      const price = "90000000000000000";
-      const settlementContractInstance = await newca.deployContract.settlement(
-        addresses,
-        proportions,
-        SongCid,
-        price
-      );
-      console.log("settlementContract deployed");
-      // set settlementContract instance
-     newca.settlementContract.load(settlementContractInstance.options.address);
-      console.log("deployContract done");
-    });
 
-  document.querySelector(".buy").addEventListener("click", async () => {
-    await newca.init();
-    const result = await newca.settlementContract.buy();
-    console.log(`buy() Transaction: ${result.transactionHash}`);
-  });
+// newca.init().then(() => {
+//   document
+//     .querySelector(".deployContracts")
+//     .addEventListener("click", async () => {
+//       await newca.init();
+//       console.log("deployContracts button clicked");
+//       // deploy SettlementContract
+//       const addresses = [
+//         // "0x1Fb5Fd68e9b34F8aF64d3B5e2D80ad9Df96F703B",
+//         // "0x9ba5f78235AB268e92615a2370a81aBB2E79C9eb",
+//         "0x21bfc39E73254320Bd652A248D35063e186901ab",
+//       ];
+//       const proportions = ["10000"];
+//       const SongCid = "QmWZipPtKnuVkn1mwNMqEw8apx513me2B3aCByCchsCmHk";
+//       const price = "90000000000000000";
+//       const settlementContractInstance = await newca.deployContract.settlement(
+//         addresses,
+//         proportions,
+//         SongCid,
+//         price
+//       );
+//       console.log("settlementContract deployed");
+//       // set settlementContract instance
+//       newca.settlementContract.load(settlementContractInstance.options.address);
+//       console.log("deployContract done");
+//     });
 
-  document.querySelector(".settle").addEventListener("click", async () => {
-    await newca.init();
-    const result = await newca.settlementContract.settle();
-    console.log(`settle() Transaction: ${result.transactionHash}`);
-  });
-});
+//   document.querySelector(".buy").addEventListener("click", async () => {
+//     await newca.init();
+//     const result = await newca.settlementContract.buy();
+//     console.log(`buy() Transaction: ${result.transactionHash}`);
+//   });
+
+//   document.querySelector(".settle").addEventListener("click", async () => {
+//     await newca.init();
+//     const result = await newca.settlementContract.settle();
+//     console.log(`settle() Transaction: ${result.transactionHash}`);
+//   });
+// });
 
 newca.init().then(() => {
+  let settleInputs = document.querySelectorAll(".settlement_deploy");
+  settleInputs[0].value = `["${newca.metamask.account}"]`;
+  settleInputs[1].value = "[10000]";
+  settleInputs[2].value = "QmWZipPtKnuVkn1mwNMqEw8apx513me2B3aCByCchsCmHk";
+  settleInputs[3].value = "10000000000000000";
   document
-    .querySelector(".deployNftContract")
+    .querySelector(".deploySettlementContract")
     .addEventListener("click", async () => {
       await newca.init();
       console.log("deploy button clicked");
-      const addresses = [
-        "0x2D96D8CC704F0f19367725c65A05Fa5Fe373fEC1",
-        "0x2D96D8CC704F0f19367725c65A05Fa5Fe373fEC1"
-      ];
-      const proportions = [5000, 5000]; 
-      const SongCid = "QmWZipPtKnuVkn1mwNMqEw8apx513me2B3aCByCchsCmHk";
-      const price = "90000000000000000";
+      // console.log(JSON.parse(settleInputs[0].value));
+      const addresses = JSON.parse(settleInputs[0].value);
+      const proportions = JSON.parse(settleInputs[1].value);
+      const SongCid = settleInputs[2].value;
+      const price = settleInputs[3].value;
       // deploy SettlementContract
       const settlementContractInstance = await newca.deployContract.settlement(
         addresses,
@@ -61,35 +64,52 @@ newca.init().then(() => {
         price
       );
       console.log("settlementContract deployed");
-      console.log("settlementContractInstance.options.address" + settlementContractInstance.options.address);
-      
-      // deploy NFT Contract
-      const dir = "QmWZipPtKnuVkn1mwNMqEw8apx513me2B3aCByCchsCmHk";
-      newca.settlementContract.load(settlementContractInstance.options.address);
-      const contract = settlementContractInstance.options.address;
-      // const contract = newca.settlementContract.instance.options.address;
-      console.log("contract : " + contract);
-      const nftContractInstance = await newca.deployContract.nft(
-        dir,
-        contract
+      console.log(
+        "settlementContractInstance.options.address" +
+          settlementContractInstance.options.address
       );
-      console.log("nftContract deployed");
-
-      newca.nftContract.load(nftContractInstance.options.address);
-      console.log("address : " + nftContractInstance.options.address)
-      const registed = await newca.nftContract.register()
+      let nftInputs = document.querySelectorAll(".nft_deploy");
+      nftInputs[0].value = "QmXmTpPtKnuVkn1mwNMqEw8apx513me2B3aCByCchsCmHk";
+      nftInputs[1].value = settlementContractInstance.options.address;
       console.log("deployContract done");
     });
+  document
+    .querySelector(".deployNftContract")
+    .addEventListener("click", async () => {
+      let nftInputs = document.querySelectorAll(".nft_deploy");
+      // deploy NFT Contract
+      const dir = nftInputs[0].value;
+      // newca.settlementContract.load(
+      //   newca.settlementContract.instance.option.address
+      // );
+      const contract = nftInputs[1].value;
+      // const contract = newca.settlementContract.instance.options.address;
+      console.log("contract : " + contract);
+      const nftContractInstance = await newca.deployContract.nft(dir, contract);
+      console.log("nftContract deployed");
+      newca.nftContract.load(nftContractInstance.options.address);
+      console.log("address : " + nftContractInstance.options.address);
+      const registed = await newca.nftContract.register();
+    });
 
-  document.querySelector(".nft_buy").addEventListener("click", async () => {
-    await newca.init();
-    const result = await newca.nftContract.buy();
-    console.log(`NFT buy() Transaction: ${result.transactionHash}`);
-  });
-
+  //nft 판매 버튼
   document.querySelector(".nft_sell").addEventListener("click", async () => {
     await newca.init();
-    const result = await newca.nftContract.sell("10000");
+    // newca.nftContract.load("0x43a53c666acf207df45838f7ef2b7511fc2f1f8d");
+    const price = document.querySelector("#nft_sell").value;
+    const result = await newca.nftContract.sell(price);
     console.log(`NFT sell() Transaction: ${result.transactionHash}`);
   });
+  //nft 구매 버튼
+  document.querySelector(".nft_buy").addEventListener("click", async () => {
+    await newca.init();
+    const price = document.querySelector("#nft_buy").value;
+    const result = await newca.nftContract.buy(price);
+    console.log(`NFT buy() Transaction: ${result.transactionHash}`);
+  });
 });
+
+//전역변수에 바인딩
+(() => {
+  window.newca = newca;
+})();
